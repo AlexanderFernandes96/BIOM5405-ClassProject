8544991bd6689e5be40ec4206a4c435a34397ba7
curtislacelle <curtislacelle@cmail.carleton.ca>
2018-11-23 23:54:29 -0500

    feature extraction including rms and derivative

---
+++ matlab/derivative_feature.m
@@ -0,0 +1,362 @@
+%derivative foot 1
+for n = 2 : 90000
+    cntlder11(n) = cntl11(n) - cntl11(n-1);
+    cntlder11(90000) = cntl11(1)-cntl11(90000);
+end
+for n = 2 : 90000
+    cntlder12(n) = cntl12(n) - cntl12(n-1);
+    cntlder12(90000) = cntl12(1)-cntl12(90000);
+end
+for n = 2 : 90000
+    cntlder13(n) = cntl13(n) - cntl13(n-1);
+    cntlder13(90000) = cntl13(1)-cntl13(90000);
+end
+for n = 2 : 90000
+    cntlder14(n) = cntl14(n) - cntl14(n-1);
+    cntlder14(90000) = cntl14(1)-cntl14(90000);
+end
+for n = 2 : 90000
+    cntlder15(n) = cntl15(n) - cntl15(n-1);
+    cntlder15(90000) = cntl15(1)-cntl15(90000);
+end
+for n = 2 : 90000
+    cntlder16(n) = cntl16(n) - cntl16(n-1);
+    cntlder16(90000) = cntl16(1)-cntl16(90000);
+end
+for n = 2 : 90000
+    cntlder17(n) = cntl17(n) - cntl17(n-1);
+    cntlder17(90000) = cntl17(1)-cntl17(90000);
+end
+for n = 2 : 90000
+    cntlder18(n) = cntl18(n) - cntl18(n-1);
+    cntlder18(90000) = cntl18(1)-cntl18(90000);
+end
+for n = 2 : 90000
+    cntlder114(n) = cntl114(n) - cntl114(n-1);
+    cntlder114(90000) = cntl114(1)-cntl114(90000);
+end
+for n = 2 : 90000
+    cntlder115(n) = cntl115(n) - cntl115(n-1);
+    cntlder115(90000) = cntl115(1)-cntl115(90000);
+end
+for n = 2 : 90000
+    cntlder116(n) = cntl116(n) - cntl116(n-1);
+    cntlder116(90000) = cntl116(1)-cntl116(90000);
+end
+
+for n = 2 : 90000
+    huntder11(n) = hunt11(n) - hunt11(n-1);
+    huntder11(90000) = hunt11(1)-hunt11(90000);
+end
+for n = 2 : 90000
+    huntder12(n) = hunt12(n) - hunt12(n-1);
+    huntder12(90000) = hunt12(1)-hunt12(90000);
+end
+for n = 2 : 90000
+    huntder13(n) = hunt13(n) - hunt13(n-1);
+    huntder13(90000) = hunt13(1)-hunt13(90000);
+end
+for n = 2 : 90000
+    huntder14(n) = hunt14(n) - hunt14(n-1);
+    huntder14(90000) = hunt14(1)-hunt14(90000);
+end
+for n = 2 : 90000
+    huntder15(n) = hunt15(n) - hunt15(n-1);
+    huntder15(90000) = hunt15(1)-hunt15(90000);
+end
+for n = 2 : 90000
+    huntder16(n) = hunt16(n) - hunt16(n-1);
+    huntder16(90000) = hunt16(1)-hunt16(90000);
+end
+for n = 2 : 90000
+    huntder17(n) = hunt17(n) - hunt17(n-1);
+    huntder17(90000) = hunt17(1)-hunt17(90000);
+end
+for n = 2 : 90000
+    huntder18(n) = hunt18(n) - hunt18(n-1);
+    huntder18(90000) = hunt18(1)-hunt18(90000);
+end
+for n = 2 : 90000
+    huntder114(n) = hunt114(n) - hunt114(n-1);
+    huntder114(90000) = hunt114(1)-hunt114(90000);
+end
+for n = 2 : 90000
+    huntder115(n) = hunt115(n) - hunt115(n-1);
+    huntder115(90000) = hunt115(1)-hunt115(90000);
+end
+for n = 2 : 90000
+    huntder116(n) = hunt116(n) - hunt116(n-1);
+    huntder116(90000) = hunt116(1)-hunt116(90000);
+end
+for n = 2 : 90000
+    huntder117(n) = hunt117(n) - hunt117(n-1);
+    huntder117(90000) = hunt117(1)-hunt117(90000);
+end
+for n = 2 : 90000
+    huntder118(n) = hunt118(n) - hunt118(n-1);
+    huntder118(90000) = hunt118(1)-hunt118(90000);
+end
+for n = 2 : 90000
+    huntder119(n) = hunt119(n) - hunt119(n-1);
+    huntder119(90000) = hunt119(1)-hunt119(90000);
+end
+for n = 2 : 90000
+    huntder120(n) = hunt120(n) - hunt120(n-1);
+    huntder120(90000) = hunt120(1)-hunt120(90000);
+end
+
+for n = 2 : 90000
+    parkder11(n) = park11(n) - park11(n-1);
+    parkder11(90000) = park11(1)-park11(90000);
+end
+for n = 2 : 90000
+    parkder12(n) = park12(n) - park12(n-1);
+    parkder12(90000) = park12(1)-park12(90000);
+end
+for n = 2 : 90000
+    parkder13(n) = park13(n) - park13(n-1);
+    parkder13(90000) = park13(1)-park13(90000);
+end
+for n = 2 : 90000
+    parkder14(n) = park14(n) - park14(n-1);
+    parkder14(90000) = park14(1)-park14(90000);
+end
+for n = 2 : 90000
+    parkder15(n) = park15(n) - park15(n-1);
+    parkder15(90000) = park15(1)-park15(90000);
+end
+for n = 2 : 90000
+    parkder16(n) = park16(n) - park16(n-1);
+    parkder16(90000) = park16(1)-park16(90000);
+end
+for n = 2 : 90000
+    parkder17(n) = park17(n) - park17(n-1);
+    parkder17(90000) = park17(1)-park17(90000);
+end
+for n = 2 : 90000
+    parkder18(n) = park18(n) - park18(n-1);
+    parkder18(90000) = park18(1)-park18(90000);
+end
+for n = 2 : 90000
+    parkder114(n) = park114(n) - park114(n-1);
+    parkder114(90000) = park114(1)-park114(90000);
+end
+for n = 2 : 90000
+    parkder115(n) = park115(n) - park115(n-1);
+    parkder115(90000) = park115(1)-park115(90000);
+end
+
+for n = 2 : 90000
+    alsder11(n) = als11(n) - als11(n-1);
+    alsder11(90000) = als11(1)-als11(90000);
+end
+for n = 2 : 90000
+    alsder12(n) = als12(n) - als12(n-1);
+    alsder12(90000) = als12(1)-als12(90000);
+end
+for n = 2 : 90000
+    alsder13(n) = als13(n) - als13(n-1);
+    alsder13(90000) = als13(1)-als13(90000);
+end
+for n = 2 : 90000
+    alsder14(n) = als14(n) - als14(n-1);
+    alsder14(90000) = als14(1)-als14(90000);
+end
+for n = 2 : 90000
+    alsder15(n) = als15(n) - als15(n-1);
+    alsder15(90000) = als15(1)-als15(90000);
+end
+for n = 2 : 90000
+    alsder16(n) = als16(n) - als16(n-1);
+    alsder16(90000) = als16(1)-als16(90000);
+end
+for n = 2 : 90000
+    alsder17(n) = als17(n) - als17(n-1);
+    alsder17(90000) = als17(1)-als17(90000);
+end
+for n = 2 : 90000
+    alsder18(n) = als18(n) - als18(n-1);
+    alsder18(90000) = als18(1)-als18(90000);
+end
+
+%derivative foot 2
+for n = 2 : 90000
+    cntlder21(n) = cntl21(n) - cntl21(n-1);
+    cntlder21(90000) = cntl21(1)-cntl21(90000);
+end
+for n = 2 : 90000
+    cntlder22(n) = cntl22(n) - cntl22(n-1);
+    cntlder22(90000) = cntl22(1)-cntl22(90000);
+end
+for n = 2 : 90000
+    cntlder23(n) = cntl23(n) - cntl23(n-1);
+    cntlder23(90000) = cntl23(1)-cntl23(90000);
+end
+for n = 2 : 90000
+    cntlder24(n) = cntl24(n) - cntl24(n-1);
+    cntlder24(90000) = cntl24(1)-cntl24(90000);
+end
+for n = 2 : 90000
+    cntlder25(n) = cntl25(n) - cntl25(n-1);
+    cntlder25(90000) = cntl25(1)-cntl25(90000);
+end
+for n = 2 : 90000
+    cntlder26(n) = cntl26(n) - cntl26(n-1);
+    cntlder26(90000) = cntl26(1)-cntl26(90000);
+end
+for n = 2 : 90000
+    cntlder27(n) = cntl27(n) - cntl27(n-1);
+    cntlder27(90000) = cntl27(1)-cntl27(90000);
+end
+for n = 2 : 90000
+    cntlder28(n) = cntl28(n) - cntl28(n-1);
+    cntlder28(90000) = cntl28(1)-cntl28(90000);
+end
+for n = 2 : 90000
+    cntlder214(n) = cntl214(n) - cntl214(n-1);
+    cntlder214(90000) = cntl214(1)-cntl214(90000);
+end
+for n = 2 : 90000
+    cntlder215(n) = cntl215(n) - cntl215(n-1);
+    cntlder215(90000) = cntl215(1)-cntl215(90000);
+end
+for n = 2 : 90000
+    cntlder216(n) = cntl216(n) - cntl216(n-1);
+    cntlder216(90000) = cntl216(1)-cntl216(90000);
+end
+
+for n = 2 : 90000
+    huntder21(n) = hunt21(n) - hunt21(n-1);
+    huntder21(90000) = hunt21(1)-hunt21(90000);
+end
+for n = 2 : 90000
+    huntder22(n) = hunt22(n) - hunt22(n-1);
+    huntder22(90000) = hunt22(1)-hunt22(90000);
+end
+for n = 2 : 90000
+    huntder23(n) = hunt23(n) - hunt23(n-1);
+    huntder23(90000) = hunt23(1)-hunt23(90000);
+end
+for n = 2 : 90000
+    huntder24(n) = hunt24(n) - hunt24(n-1);
+    huntder24(90000) = hunt24(1)-hunt24(90000);
+end
+for n = 2 : 90000
+    huntder25(n) = hunt25(n) - hunt25(n-1);
+    huntder25(90000) = hunt25(1)-hunt25(90000);
+end
+for n = 2 : 90000
+    huntder26(n) = hunt26(n) - hunt26(n-1);
+    huntder26(90000) = hunt26(1)-hunt26(90000);
+end
+for n = 2 : 90000
+    huntder27(n) = hunt27(n) - hunt27(n-1);
+    huntder27(90000) = hunt27(1)-hunt27(90000);
+end
+for n = 2 : 90000
+    huntder28(n) = hunt28(n) - hunt28(n-1);
+    huntder28(90000) = hunt28(1)-hunt28(90000);
+end
+for n = 2 : 90000
+    huntder214(n) = hunt214(n) - hunt214(n-1);
+    huntder214(90000) = hunt214(1)-hunt214(90000);
+end
+for n = 2 : 90000
+    huntder215(n) = hunt215(n) - hunt215(n-1);
+    huntder215(90000) = hunt215(1)-hunt215(90000);
+end
+for n = 2 : 90000
+    huntder216(n) = hunt216(n) - hunt216(n-1);
+    huntder216(90000) = hunt216(1)-hunt216(90000);
+end
+for n = 2 : 90000
+    huntder217(n) = hunt217(n) - hunt217(n-1);
+    huntder217(90000) = hunt217(1)-hunt217(90000);
+end
+for n = 2 : 90000
+    huntder218(n) = hunt218(n) - hunt218(n-1);
+    huntder218(90000) = hunt218(1)-hunt218(90000);
+end
+for n = 2 : 90000
+    huntder219(n) = hunt219(n) - hunt219(n-1);
+    huntder219(90000) = hunt219(1)-hunt219(90000);
+end
+for n = 2 : 90000
+    huntder220(n) = hunt220(n) - hunt220(n-1);
+    huntder220(90000) = hunt220(1)-hunt220(90000);
+end
+
+for n = 2 : 90000
+    parkder21(n) = park21(n) - park21(n-1);
+    parkder21(90000) = park21(1)-park21(90000);
+end
+for n = 2 : 90000
+    parkder22(n) = park22(n) - park22(n-1);
+    parkder22(90000) = park22(1)-park22(90000);
+end
+for n = 2 : 90000
+    parkder23(n) = park23(n) - park23(n-1);
+    parkder23(90000) = park23(1)-park23(90000);
+end
+for n = 2 : 90000
+    parkder24(n) = park24(n) - park24(n-1);
+    parkder24(90000) = park24(1)-park24(90000);
+end
+for n = 2 : 90000
+    parkder25(n) = park25(n) - park25(n-1);
+    parkder25(90000) = park25(1)-park25(90000);
+end
+for n = 2 : 90000
+    parkder26(n) = park26(n) - park26(n-1);
+    parkder26(90000) = park26(1)-park26(90000);
+end
+for n = 2 : 90000
+    parkder27(n) = park27(n) - park27(n-1);
+    parkder27(90000) = park27(1)-park27(90000);
+end
+for n = 2 : 90000
+    parkder28(n) = park28(n) - park28(n-1);
+    parkder28(90000) = park28(1)-park28(90000);
+end
+for n = 2 : 90000
+    parkder214(n) = park214(n) - park214(n-1);
+    parkder214(90000) = park214(1)-park214(90000);
+end
+for n = 2 : 90000
+    parkder215(n) = park215(n) - park215(n-1);
+    parkder215(90000) = park215(1)-park215(90000);
+end
+
+for n = 2 : 90000
+    alsder21(n) = als21(n) - als21(n-1);
+    alsder21(90000) = als21(1)-als21(90000);
+end
+for n = 2 : 90000
+    alsder22(n) = als22(n) - als22(n-1);
+    alsder22(90000) = als22(1)-als22(90000);
+end
+for n = 2 : 90000
+    alsder23(n) = als23(n) - als23(n-1);
+    alsder23(90000) = als23(1)-als23(90000);
+end
+for n = 2 : 90000
+    alsder24(n) = als24(n) - als24(n-1);
+    alsder24(90000) = als24(1)-als24(90000);
+end
+for n = 2 : 90000
+    alsder25(n) = als25(n) - als25(n-1);
+    alsder25(90000) = als25(1)-als25(90000);
+end
+for n = 2 : 90000
+    alsder26(n) = als26(n) - als26(n-1);
+    alsder26(90000) = als26(1)-als26(90000);
+end
+for n = 2 : 90000
+    alsder27(n) = als27(n) - als27(n-1);
+    alsder27(90000) = als27(1)-als27(90000);
+end
+for n = 2 : 90000
+    alsder28(n) = als28(n) - als28(n-1);
+    alsder28(90000) = als28(1)-als28(90000);
+end
+    
